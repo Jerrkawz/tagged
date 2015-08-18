7db730eb25d329d40f32cae6432e5632a9f6d9c4
Jerrod <jllankfo@ncsu.edu>
2015-08-18 14:30:32 -0400

    Added tags/posts also added bower

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
+  has_and_belongs_to_many :tags
   belongs_to :user
 
-  validates :name, presence: true, length: {maximum: 250}, allow_blank: false, allow_nil: false
+  validates :title, presence: true, length: {maximum: 250}, allow_blank: false, allow_nil: false
   validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805022409) do
+ActiveRecord::Schema.define(version: 20150818172926) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20150805022409) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "posts_tags", id: false, force: true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
